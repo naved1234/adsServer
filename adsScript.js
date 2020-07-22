993aa97f950a31982426ae5ea1a540e4cabda5dc
Naved Ahmad <naved@babydestination.com>
2020-07-22 17:23:51 +0530

    removed extra adscript

--- adsScript.js
+++
@@ -1,8 +0,0 @@
-const insertAd = () => {
-  const adDiv = document.getElementById('some_random_id');
-  adDiv.style.backgroundImage = "url('http://localhost:3000/images/kellogs.jpg')";
-  adDiv.style.width = "500px";
-  adDiv.style.height = "400px";
-};
-
-insertAd();
